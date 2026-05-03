2f4533da746571759b9ef79d008c60524f059ff2
Carlo Segat (TUB) <carlo.segat@tu-berlin.de>
2026-05-03 19:20:14 +0200

    fix(removed all_links)
    realized that go_router handles passign the link to the router

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'dart:async';
-
-import 'package:app_links/app_links.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -37,50 +34,8 @@ final _routerConfig = GoRouter(
   ],
 );
 
-class TodoApp extends StatefulWidget {
+class TodoApp extends StatelessWidget {
   const TodoApp({super.key});
-
-  @override
-  State<TodoApp> createState() => _TodoAppState();
-}
-
-class _TodoAppState extends State<TodoApp> {
-  final _appLinks = AppLinks();
-
-  // just holds the "subscription" object to the appLinks "bridge"
-  StreamSubscription<Uri>? _sub;
-
-  @override
-  void initState() {
-    super.initState();
-    _initDeepLinks();
-  }
-
-  Future<void> _initDeepLinks() async {
-    // deeplinks have the so called "cold start problem"
-    // app already running when deeplink pressed --> all good / "warm" start
-    // app NOT already running (i.e. app is launched by the OS after deeplink pressed) --> cold start
-
-    // if it's cols start the stream will not fire the event
-    final initial = await _appLinks.getInitialLink();
-    if (initial != null) _handleDeeplink(initial);
-
-    // everytime the native part of our app recives a link from the OS, please call `_handle`
-    _sub = _appLinks.uriLinkStream.listen(_handleDeeplink);
-  }
-
-  void _handleDeeplink(Uri uri) {
-    if (uri.scheme != 'todoapp') return;
-    if (uri.host == 'task' && uri.pathSegments.isNotEmpty) {
-      _routerConfig.go('/task/${uri.pathSegments.first}');
-    }
-  }
-
-  @override
-  void dispose() {
-    _sub?.cancel();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
